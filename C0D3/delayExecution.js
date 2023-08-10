94e61b011b3bcee1cc31c9e9fb7b286e941df88a
ruwen <ruwen.yi@adelaide.edu.au>
2023-08-10 21:42:38 +0800

    update comments

--- C0D3/delayExecution.js
+++ C0D3/delayExecution.js
@@ -11,25 +11,29 @@ should print out:
 levellevellevel
 */
 
+/* 
 const a = {
     "jayZ": (key) => console.log(key),
     "tupac": (key) => console.log(key + 2),
     "name3": (key) => console.log(key === "name3"),
     "level": (key) => console.log(key + key + key)
- }
- 
- solution(a, 5000)
- 
- function solution(obj, sec){
-   let keys = Object.keys(obj);
-   let length = keys.length;
- 
-   function executeNext(index){
-     if (index >= length) return;
- 
-     obj[keys[index]](keys[index]);
-     setTimeout(()=>executeNext(index+1), sec);
-   }
- 
-   executeNext(0);
- }
+}
+
+solution(a, 5000) 
+*/
+
+function solution(obj, sec) {
+    let keys = Object.keys(obj);
+    let length = keys.length;
+
+    if (!length) return;
+
+    function executeNext(index) {
+        if (index >= length) return;
+
+        obj[keys[index]](keys[index]);
+        setTimeout(() => executeNext(index + 1), sec);
+    }
+
+    executeNext(0);
+}

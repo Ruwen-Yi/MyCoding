edfc5ba98f3517ff32ba5ef35cf333c2b4d86b24
ruwen <ruwen.yi@adelaide.edu.au>
2023-08-14 22:00:14 +0800

    upload code

---
+++ C0D3/duplicateNum.js
@@ -0,0 +1,26 @@
+/* 
+Source：https://www.c0d3.com/curriculum/js3?challenge=95
+
+Duplicate Numbers
+Write a function that takes in an array of numbers, and returns a new array of all duplicate numbers.
+
+solution([1, 2, 2, 3, 1])  // should return [1, 2]
+solution([1, 1, 1, 1, 2, 3, 3])  // should return [3, 1]
+*/
+
+function solution(arr) {
+  let count = arr.reduce((acc, item, index)=>{
+    acc[item] = (acc[item] || 0) + 1;
+    return acc;
+  },{})
+
+  // console.log(count);
+
+  let duplicatedNum = [];
+  for (let key in count) {
+    if(count[key] > 1) duplicatedNum.push(key);
+  }
+
+  return duplicatedNum;
+}
+solution([1, 2, 2, 3, 1])

cbb3669b65e6ea1b00fe7ca2320883910788710c
ruwen <ruwen.yi@hotmail.com>
2023-08-16 11:18:39 +0800

    upload code and test

---
+++ C0D3/findTwoSum.js
@@ -0,0 +1,24 @@
+/* 
+Source: https://www.c0d3.com/curriculum/js3?challenge=94
+
+Write a function that takes in an array of numbers and a number, and returns true if any pairs add up to the number. (The numbers in the array is not unique, meaning there may be duplicate numbers)
+
+solution([1,2,22,333,23], 25)   // returns true
+solution([1,2,22,333,23], 24)   // returns true
+*/
+
+function solution(arr, targetSum) {
+    const set = new Set();
+
+    for (let num of arr) {
+        let complement = targetSum - num;
+        
+        if (set.has(complement)) return true;
+        
+        set.add(num);
+    }
+
+    return false;
+}
+
+module.exports = {solution};
---
+++ C0D3/tests/findTwoSum.test.js
@@ -0,0 +1,28 @@
+const { solution } = require('../findTwoSum'); // 假设 solution 函数位于一个名为 solution.js 的文件中
+
+describe('solution function', () => {
+    it('should return true if there are two numbers that add up to the target sum', () => {
+        const result = solution([1, 2, 22, 333, 23], 25);
+        expect(result).toBe(true);
+    });
+
+    it('should return true if there are two numbers that add up to the target sum', () => {
+        const result = solution([1, 2, 22, 333, 23], 24);
+        expect(result).toBe(true);
+    });
+
+    it('should return false if there are no two numbers that add up to the target sum', () => {
+        const result = solution([1, 2, 22, 333, 23], 100);
+        expect(result).toBe(false);
+    });
+
+    it('should handle negative numbers', () => {
+        const result = solution([-1, 2, 3, 4, 5], 1);
+        expect(result).toBe(true);
+    });
+
+    it('should handle an array with duplicate numbers', () => {
+        const result = solution([1, 1, 1, 2, 2, 3, 3], 4);
+        expect(result).toBe(true);
+    });
+});

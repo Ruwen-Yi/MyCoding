b533239f8464566cea112b4115e70798d201117e
ruwen <ruwen.yi@hotmail.com>
2023-08-24 22:20:44 +0800

    feat(css):set layout

--- C0D3/FrontEndEngineering/LightsOffGame/lightsOffGame.js
+++ C0D3/FrontEndEngineering/LightsOffGame/lightsOffGame.js
@@ -1,4 +1,8 @@
-let light = document.getElementsByClassName('light')[0];
-light.onclick = ()=>{
-    light.classList.toggle('light-on')
+let lightContainer = document.getElementsByClassName('light-table')[0];
+
+// click to light on/off
+lightContainer.onclick = (e)=>{
+    if (e.target.classList.contains('light')) {
+        e.target.classList.toggle('light-on');
+    }
 };
